2acc7447184ab38fc21d029e9385dc3df262a658
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-05-16 17:59:00 +0200

    [fix] Text Color on Show Page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.main')
 
 @section('content')
-  <h1 class="text-center fw-bold py-5">{{ $comic->title }}</h1>
-  <div class="d-flex">
+  <h1 class="text-center fw-bold py-5 text-white">{{ $comic->title }}</h1>
+  <div class="d-flex text-white">
     <div class="w-50 px-5">
       <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
     </div>
@@ -35,5 +35,5 @@
 
     </div>
   </div>
-  <p class="p-5"><strong>Descrizione: </strong>{{ $comic->description }}</p>
+  <p class="p-5 text-white"><strong>Descrizione: </strong>{{ $comic->description }}</p>
 @endsection
